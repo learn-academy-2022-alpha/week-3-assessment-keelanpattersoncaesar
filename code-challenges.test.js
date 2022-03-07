f894c925096ea9b284e174231faeec8be1b14258
keelanpattersoncaesar <keelanpattersoncaesar@gmail.com>
2022-03-07 08:27:30 -0800

    finished code challenges and interviews

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -21,23 +21,23 @@ describe("fibSequence", () => {
     })
   })
 
-  // Good failure! Hooray
+//   // Good failure! Hooray
 
-// Example input: 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+// // Example input: 6
+// // Expected output: [1, 1, 2, 3, 5, 8]
 
-// Example input: 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+// // Example input: 10
+// // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 
-// b) Create the function that makes the test pass.
+// // b) Create the function that makes the test pass.
 
-// PC:
-// 
+// // PC:
+// // 
 
-const fibSequence = (number) => {
-    return number 
-}
+// const fibSequence = (number) => {
+//     return number 
+// }
 
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
@@ -50,16 +50,46 @@ const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
 
+describe("oddOnly", () => {
+  it("takes in an array and returns a new array of only odd numbers sorted from least to greatest", () => {
+    expect(oddOnly(fullArr1)).toEqual([-9, 7, 9, 199])
+    expect(oddOnly(fullArr2)).toEqual([-823, 7, 23])
+  })
+})
+
+// GOOD FAILURE!
 
 // b) Create the function that makes the test pass.
 
+// PS: 
+// create oddOnly function
+  // takes array as parameter
+// .filter on any value that is NOT a number
+// evaluate each by modulo 2 (!== %2 for odds)
+// array.sort()
+// call function
+// big success
+
+// function oddOnly(array){
+//   if (array % 2 !== 0) {
+//     // array.filter
+//     console.log(oddOnly);
+//   }
+
+// const filter = array.filter((number) => number % 2)
+
+// function oddOnly(number){
+//   if (typeof(number) === 'number'){
+//     console.log(oddOnly) 
+//   }
+// }
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
 const numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
+// Expected output: [2, 6, 51, 60]
 
 const numbersToAdd2 = [0, 7, -8, 12]
 // Expected output: [0, 7, -1, 11]
@@ -67,5 +97,32 @@ const numbersToAdd2 = [0, 7, -8, 12]
 const numbersToAdd3 = []
 // Expected output: []
 
+describe("arraySum", () => {
+  it("takes in an array and returns an array of the accumulating sum. An empty array should return an empty array", () => {
+    expect(arraySum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+    expect(arraySum(numbersToAdd2)).toEqual([0, 7, -1, 11])
+    expect(arraySum(numbersToAdd3)).toEqual([])
+  })
+})
+
+// The best failing possible
 
 // b) Create the function that makes the test pass.
+
+// PS: 
+// create arraySum function
+// takes in array
+// create empty array
+// add each index to the next (including previous values)
+  // i[0] + i[1], i[0] + i[1] + i[2] etc
+    // .reduce(previous value, current value, index) method
+// call function
+
+function arraySum(array){
+  let array2 = [];
+  return array.reduce((previous, current, i) => array2[i] = previous+current, 0)
+}
+
+arraySum(numbersToAdd1)
+arraySum(numbersToAdd2)
+arraySum(numbersToAdd3)
